7cf168fd78aa33fd26efa13a2c46ef5e64feb710
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-21 05:32:16 -0800

    removed trailing line

--- exec_file.c
+++ exec_file.c
@@ -9,12 +9,13 @@
 *
 * Return: nothing
 */
+
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-				{"pop", f_pop},{"swap", f_swap},{"add", f_add},
+				{"pop", f_pop}, {"swap", f_swap}, {"add", f_add},
 				{"nop", f_nop},
 				{"sub", f_sub},
 				{"div", f_div},
